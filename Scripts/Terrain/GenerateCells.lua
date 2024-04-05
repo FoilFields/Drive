03de985f98d6facfeb7bba7a797ea3b574efdd7c
Woukie <lucaseldon@live.com>
2024-04-05 14:12:26 +0100

    Added starter house tile

--- Scripts/Terrain/GenerateCells.lua
+++ Scripts/Terrain/GenerateCells.lua
@@ -7,26 +7,23 @@ function generateOverworldCelldata(xMin, xMax, yMin, yMax, seed, data, padding)
     math.randomseed(seed)
 
     initializeCellData(xMin, xMax, yMin, yMax, seed)
-    
-    -- Keeping this cos cba to make own system
-    g_cornerTemp = {
-        type = {},
-    }
-    for y = yMin, yMax + 1 do
-        g_cornerTemp.type[y] = {}
-        for x = xMin, xMax + 1 do
-            g_cornerTemp.type[y][x] = TYPE_DESERT 
-        end
-    end
 
-    -- Set padding to scorched
+    -- Set padding to scorched and everything else to desert
     for x = xMin, xMax do
         for y = yMin, yMax do
             if (x < xMin + padding or x > xMax - padding or y < yMin + padding or y > yMax - padding) then
                 local tileId = getScorchedTileId(sm.noise.intNoise2d( x, y, g_cellData.seed + 1234 ))
                 if not tileId:isNil() then
                     g_cellData.uid[y][x] = tileId
-                    g_cellData.rotation[y][x] = math.random(0, 4)
+                    g_cellData.rotation[y][x] = sm.noise.intNoise2d( x, y, g_cellData.seed ) % 4
+                    g_cellData.xOffset[y][x] = 0
+                    g_cellData.yOffset[y][x] = 0
+                end
+            else
+                local tileId = getDesertTileId(sm.noise.intNoise2d( x, y, g_cellData.seed + 6004 ))
+                if not tileId:isNil() then
+                    g_cellData.uid[y][x] = tileId
+                    g_cellData.rotation[y][x] = sm.noise.intNoise2d( x, y, g_cellData.seed ) % 4
                     g_cellData.xOffset[y][x] = 0
                     g_cellData.yOffset[y][x] = 0
                 end
@@ -85,34 +82,34 @@ function generateOverworldCelldata(xMin, xMax, yMin, yMax, seed, data, padding)
     -- Elevation
     forEveryCorner( function( x, y )
         local elevation = 0.1
-        elevation = elevation + (sm.noise.perlinNoise2d( x / 64, y / 64, seed + 12032 ) + 1) * 2 -- Super duper scrolling terrain
-        elevation = elevation + (sm.noise.perlinNoise2d( x / 32, y / 32, seed + 10293 ) + 1)
-        elevation = elevation + (sm.noise.perlinNoise2d( x / 16, y / 16, seed + 7907 ) + 1) * 0.75
-        elevation = elevation + (sm.noise.perlinNoise2d( x / 8, y / 8, seed + 5527 ) + 1) * 0.5
-        elevation = elevation + (sm.noise.perlinNoise2d( x / 4, y / 4, seed + 8733 ) + 1) * 0.25
-        elevation = elevation + (sm.noise.perlinNoise2d( x / 2, y / 2, seed + 5442 ) + 1) * 0.125
+        elevation = elevation + (sm.noise.perlinNoise2d( x / 128, y / 128, seed + 14123 ) + 0.5) * 1.5 -- Super duper scrolling terrain
+        elevation = elevation + (sm.noise.perlinNoise2d( x / 64, y / 64, seed + 12032 ) + 0.5) * 1.25
+        elevation = elevation + (sm.noise.perlinNoise2d( x / 32, y / 32, seed + 10293 ) + 0.5)
+        elevation = elevation + (sm.noise.perlinNoise2d( x / 16, y / 16, seed + 7907 ) + 0.5) * 0.75
+        elevation = elevation + (sm.noise.perlinNoise2d( x / 8, y / 8, seed + 5527 ) + 0.5) * 0.5
+        elevation = elevation + (sm.noise.perlinNoise2d( x / 4, y / 4, seed + 8733 ) + 0.5) * 0.25
+        elevation = elevation + (sm.noise.perlinNoise2d( x / 2, y / 2, seed + 5442 ) + 0.5) * 0.125
         g_cellData.elevation[y][x] = elevation * 250 / 3
-
-        print(sm.noise.perlinNoise2d( x / 2, y / 2, seed ))
 	end )
 
-    local poi = {
-        x = 0,
-        y = 0,
-        type = POI_ROAD,
-        size = 1,
-        road = false,
-        flat = false,
-        terrainType = TYPE_DESERT,
-        edges = {}
-    }
+    -- Starter house
+    g_cellData.uid[0][1] = getPoi(sm.noise.intNoise2d( 1, 0, g_cellData.seed + 1032 ), "StarterHouse")
+    g_cellData.rotation[0][1] = 0
+    g_cellData.xOffset[0][1] = 0
+    g_cellData.yOffset[0][1] = 0
 
-    writePoi(poi)
+    -- Flattern starter house
+    for x = -1, 1, 1 do
+        for y = -1, 1, 1 do
+            g_cellData.elevation[0 + y][1 + x] = g_cellData.elevation[0][1]
+        end
+    end
 
-    -- evaluate deserts
-	evaluateType( TYPE_DESERT, getDesertTileIdAndRotation )    
+    -- Elevator
 
+    -- on-road pois
 
-    -- clean up
-    g_cornerTemp = nil
+    -- road-side pois
+
+    -- far-off pois
 end
