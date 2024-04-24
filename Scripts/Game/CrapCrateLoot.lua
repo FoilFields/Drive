2dc4b26468cd5432316b87c5b348e826f1242d71
Woukie <lucaseldon@live.com>
2024-04-24 13:08:59 +0100

    Updated crap loot

--- Scripts/Game/CrapCrateLoot.lua
+++ Scripts/Game/CrapCrateLoot.lua
@@ -1,14 +1,14 @@
 dofile("$SURVIVAL_DATA/Scripts/game/survival_items.lua")
 
-g_crap_pools = {
+g_crap_crate_pools = {
   weight = 0,
   draws = {
+    {uid = obj_consumable_gas, stackSize = 1, weight = 30}, 
     {uid = obj_consumable_gas, stackSize = 2, weight = 25}, 
-    {uid = obj_consumable_gas, stackSize = 3, weight = 25}, 
-    {uid = obj_consumable_gas, stackSize = 4, weight = 15}, 
+    {uid = obj_consumable_gas, stackSize = 3, weight = 15}, 
+    {uid = obj_consumable_component, stackSize = 1, weight = 30}, 
     {uid = obj_consumable_component, stackSize = 2, weight = 25}, 
-    {uid = obj_consumable_component, stackSize = 3, weight = 25}, 
-    {uid = obj_consumable_component, stackSize = 4, weight = 15}, 
+    {uid = obj_consumable_component, stackSize = 3, weight = 15}, 
     {uid = tool_connect, stackSize = 1, weight = 10}, 
     {uid = tool_paint, stackSize = 1, weight = 10}, 
     {uid = tool_weld, stackSize = 1, weight = 10}, 
@@ -18,17 +18,17 @@ g_crap_pools = {
   }
 }
 
-if g_crap_pools.weight == 0 then
-  for _, item in ipairs(g_crap_pools.draws) do
-    g_crap_pools.weight = g_crap_pools.weight + item.weight
+if g_crap_crate_pools.weight == 0 then
+  for _, item in ipairs(g_crap_crate_pools.draws) do
+    g_crap_crate_pools.weight = g_crap_crate_pools.weight + item.weight
   end
 end
 
 function GetRandomCrap()
-  local weight = g_crap_pools.weight
+  local weight = g_crap_crate_pools.weight
 
-  for i = 1, #g_crap_pools.draws, 1 do
-    local crap = g_crap_pools.draws[i]
+  for i = 1, #g_crap_crate_pools.draws, 1 do
+    local crap = g_crap_crate_pools.draws[i]
 
     if math.random(weight) <= crap.weight then
       return crap
